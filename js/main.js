b2c94ee753cd5a167d948ea3f505568c65d9a992
nebelschwaden <ronny_jaramillo123@hotmail.es>
2021-01-28 00:43:53 -0500

    General structure built.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,13 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
