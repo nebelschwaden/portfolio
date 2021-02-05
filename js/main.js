69a244b7fb5da370a0e2dd48db9eab39251dda8e
nebelschwaden <ronny_jaramillo123@hotmail.es>
2021-02-05 18:35:31 -0500

    Deleted unnecessary files and made some fixes.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
+/* Navbar Collapsible */
 const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
   })
 );
-window.onscroll = function() {myFunction()};
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+/* Video Modal */
+let modal = document.getElementById("myModal");
+let videoButtons = document.querySelectorAll(".video__btn");
+let span = document.querySelectorAll(".close")[0];
+let modalVideo = document.querySelector('.modal__video');
+let navBar = document.querySelector('.nav');
+videoButtons.forEach((btn) => {
+  btn.onclick = function () {
+    modalVideo.src = 'videos/'+btn.id+'.mp4';
+    modal.style.display = "block";
+    navBar.style.visibility = 'hidden';
+  }
+});
+span.onclick = function() {
+  modal.style.display = "none";
+  modalVideo.pause();
+  navBar.style.visibility = 'visible';
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    modalVideo.pause();
+    navBar.style.visibility = 'visible';
   }
 }
